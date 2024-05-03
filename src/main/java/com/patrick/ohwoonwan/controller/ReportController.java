da51cfb138f7769192cb26af380effb476995adc
HyeonAm <oha949500@gmail.com>
2024-05-03 09:43:19 +0900

    ReportController 오타 수정

--- src/main/java/com/patrick/ohwoonwan/controller/ReportController.java
+++ src/main/java/com/patrick/ohwoonwan/controller/ReportController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/report")
 @RequiredArgsConstructor
-public class ReportdController {
+public class ReportController {
 
     private final ReportService reportService;
 
